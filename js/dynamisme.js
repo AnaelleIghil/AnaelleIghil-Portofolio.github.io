242f38d15a98376bdd5da907e6f1d1c5e7d3ac5f
AnaelleIghil <anaelle.ighil@gmail.com>
2022-12-03 16:42:47 +0100

    echec dynamsime

--- js/dynamisme.js
+++ js/dynamisme.js
@@ -1,9 +1,26 @@
-const contenu = document.querySelector('#content');
-console.log(contenu);
+/* const contenu = document.querySelector('#content');
+
 function handleMoveClick(event){
     console.log(event.target);
     if(event.target.value){
-        event.target.backgroundPosition = left;
-    }
+        for(let move=0; move<contenu.lenght; move++ ){
+            move = value.style.backgroundPosition= left;
+        }
+}
 }
 contenu.addEventListener('click', handleMoveClick);
+ */
+// echec sans jQuery.
+const x = document.querySelectorAll('section');
+
+
+ function handleShowHide(){
+
+    if (x.target.style.display === "none"){
+        x.target.style.display ="block";
+    } else {
+        x.target.style.display= "none";
+    }
+}
+
+x.addEventListener('click', handleShowHide()); 
