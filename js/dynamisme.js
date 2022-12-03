d6bece5cf7f933f8880eea8ffe8169a269b72b96
AnaelleIghil <anaelle.ighil@gmail.com>
2022-12-03 13:56:25 +0100

    creation js, modification html main en form pour Bubbling

---
+++ js/dynamisme.js
@@ -0,0 +1,6 @@
+// créer function qui va modifier la position du background en fonction des catégorie cliqué
+
+const milieu = document.querySelectorAll('#content_middle');
+const droit =document.querySelectorAll('#content_right');
+const gauche = document.querySelectorAll('#content_left');
+
