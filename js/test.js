18095f765398321680d62f3f87fe9d8d2a50313a
AnaelleIghil <anaelle.ighil@gmail.com>
2022-12-24 14:36:40 +0100

    work in progress

--- js/test.js
+++ js/test.js
@@ -1,5 +1,4 @@
 const containerPast= document.createElement('div');
-containerPast.appendChild(body);
 containerPast.className= 'past';
 const containerPastCinema = document.createElement('div');
-containerPastCinema.appencChild(containerPast);
+containerPastCinema.appendChild(containerPast);
