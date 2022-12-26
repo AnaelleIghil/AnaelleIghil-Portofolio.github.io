707a4606a69179fd36d2042a28dd1e7c50d40a68
AnaelleIghil <anaelle.ighil@gmail.com>
2022-12-26 17:51:22 +0100

    js

--- js/test.js
+++ js/test.js
@@ -1,12 +1,10 @@
-const mainDiv = document.getElementsByClassName('main');
-const divDisplay= document.querySelector('subclass');
-
-mainDiv.addEventListener('click', divDisplayHandler())
-
-function divDisplayHandler(event){
-    if(divDisplay.style.display=== 'none'){
-        divDisplay.style.display = 'block'
-     } else {
-        console.log('tout est déjà affiché')
-     }
+const list = document.getElementsByTagName("ul");
+for(const i=0; i<list.length; i++){
+    list[i].addEventListener('click', liClick);
+}
+function liClick(){
+    const listItem= document.getElementsByTagName("li");
+    for(const j=0; j< listItem.length; j++){
+        this.style.display = 'block';
+    }
 }
